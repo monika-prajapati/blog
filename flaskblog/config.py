0e9dade21b2630f97de3e8dd8afd2d7e0c5f802a
Monika Prajapati <monicaprjpt@gmail.com>
2021-03-21 17:02:20 +0545

    Add a point to add new projects and other minor changes

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -2,10 +2,10 @@ import os
 
 class Config:
     SECRET_KEY='0f622755b7cb1481e76c31e396436a4c'
-    SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
+    # SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://bloguser:admin@localhost/Flaskblog'
     MAIL_SERVER='smpt.googlemail.com'
     MAIL_PORT=587
     MAIL_USE_TLS=True
     MAIL_USERNAME=os.environ.get('EMAIL_USER')
     MAIL_PASSWORD=os.environ.get('EMAIL_PASS')
-    
